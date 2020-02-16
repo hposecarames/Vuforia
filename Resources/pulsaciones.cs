1bc2e246db49344c367a84dfcec7713f0de41c50
Hector <hposecarames@danielcastelao.org>
2020-02-16 23:59:24 +0100

    Commit inicial

---
+++ Resources/pulsaciones.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class pulsaciones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshPro textMesh;
+    public string texto;
+    void Start()
+    {
+        te = GetComponent<TextMeshPro>();
+        texto = "Prueba";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Touch touch in Input.touches){
+        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+        RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "Cube")
+                {
+                    texto = "Vale Jonathan";
+
+                }
+            }
+        }
+    }
+}
